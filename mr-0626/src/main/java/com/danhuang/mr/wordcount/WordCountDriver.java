af3e2e4f919c81ccceb1d5236b901f2f31ef141b
Godwithegg <675175647@qq.com>
2019-06-27 01:01:25 +0800

    update

---
+++ mr-0626/src/main/java/com/danhuang/mr/wordcount/WordCountDriver.java
@@ -0,0 +1,16 @@
+package com.danhuang.mr.wordcount;
+
+public class WordCountDriver {
+    public static void main(String[] args) {
+
+        //1.获取Job对象
+
+        //2.设置jar存储位置
+
+        //3.关联Map和Reduce类
+
+        //4.设置mapper阶段输出数据的key和value类型
+
+        //5.设置最终数据输出的key和value类型
+    }
+}
